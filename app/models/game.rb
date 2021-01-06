77b0880ba3ace6f6bbd1e1a57136c4228b00e3b0
Seigo Jinbo <seigojinbo@gmail.com>
2021-01-06 10:32:26 +0900

    show dice / mask dice methods

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,18 +2,43 @@ class Game < ApplicationRecord
   has_many :sessions
   has_many :users, through: :sessions
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
-
+  attr_accessor :state
   def new_game(state = {})
+    @test = 'test'
     count = 1
     users.each do |user|
-      state[:"player#{count}"] = [1, 1, 1, 1, 1]
+      state[:"player#{count}"] = { name: user.email, dice: roll(6) }
       count += 1
     end
-    return state
-    raise
+    @state = state
+    return @state
   end
 
-  # def show_users()
-  #   users.each { |user| user.email }
-  # end
+  def roll(total = 6)
+    dice = []
+    total.times { dice << rand(1..6) }
+    return dice
+  end
+
+  def show_all_dice()
+    array = []
+    state.each { |key, value| array << value[:dice] }
+    # player = state.find { |key, hash| hash['name'] == "#{user.email}" }
+    return array
+  end
+
+  def show_your_dice(user)
+    array = []
+    state.each do |key, value|
+      if value[:name] == user.email
+        array << value[:dice]
+      else
+        mask_array = []
+        value[:dice].length.times { mask_array << '*' }
+        array << mask_array
+      end
+    end
+    # player = state.find { |key, hash| hash['name'] == "#{user.email}" }
+    return array
+  end
 end

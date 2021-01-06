a202992fb38f2bdae431ab70aafcc9cb536e163b
Seigo Jinbo <seigojinbo@gmail.com>
2021-01-06 11:15:10 +0900

    round loss / new round method

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,11 +3,11 @@ class Game < ApplicationRecord
   has_many :users, through: :sessions
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
   attr_accessor :state
-  def new_game(state = {})
+  def new_game(state = [])
     @test = 'test'
     count = 1
     users.each do |user|
-      state[:"player#{count}"] = { name: user.email, dice: roll(6) }
+      state << { name: user.email, dice: roll(5) }
       count += 1
     end
     @state = state
@@ -22,23 +22,32 @@ class Game < ApplicationRecord
 
   def show_all_dice()
     array = []
-    state.each { |key, value| array << value[:dice] }
+    state.each { |player| array << player[:dice] }
     # player = state.find { |key, hash| hash['name'] == "#{user.email}" }
     return array
   end
 
   def show_your_dice(user)
     array = []
-    state.each do |key, value|
-      if value[:name] == user.email
-        array << value[:dice]
+    state.each do |player|
+      if player[:name] == user.email
+        array << player[:dice]
       else
         mask_array = []
-        value[:dice].length.times { mask_array << '*' }
+        player[:dice].length.times { mask_array << '*' }
         array << mask_array
       end
     end
     # player = state.find { |key, hash| hash['name'] == "#{user.email}" }
     return array
+  end
+
+  def round_loss(user)
+    player = state.find { |player| player[:name] == user.email }
+    player[:dice].pop
+  end
+
+  def new_round
+    state.each { |player| player[:dice] = roll(:dice.count) }
   end
 end

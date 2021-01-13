2d782dfaa0d6f6a0570208d3065cad3f0de77c5f
Seigo Jinbo <seigojinbo@gmail.com>
2021-01-13 22:59:13 +0900

    renders same output on every browser...

--- app/javascript/channels/game_channel.js
+++ app/javascript/channels/game_channel.js
@@ -1,5 +1,5 @@
 import consumer from "./consumer";
-
+// var my_var = <%= User.first.email %>
 const initGameCable = () => {
   const gameContainer = document.getElementById("game");
 
@@ -7,33 +7,42 @@ const initGameCable = () => {
     const id = gameContainer.dataset.gameId;
     const players = document.querySelectorAll(".player");
     const action = document.querySelector(".action");
+
     consumer.subscriptions.create(
       { channel: "GameChannel", id: id },
       {
         received(data) {
-          console.log(data.split(" ")[4].trim() === 'class="simple_form');
+          const turn = document.querySelector(".turn");
+          const currentTurn = turn.dataset.currentTurn;
+          if (currentTurn === "true") {
+            console.log("yes");
+          }
+          if (currentTurn === "false") {
+            console.log("no");
+          }
+          // console.log(currentTurn);
+          // console.log(data.split(" "));
+          // console.log(data.split(" ")[1].trim() == 'class="action-content"');
           if (data[4] === "c") {
             const currentBet = document.querySelector(".current-bet");
             currentBet.innerHTML = data;
-          }
-          // if (data[12] === "b") {
-          //   gameContainer.innerHTML = data;
-          // } else {
-          else if (
+          } else if (
             data.split(" ")[1].trim() === "loses" ||
-            data.split(" ")[0].trim() === "<p><i" ||
-            data.split(" ")[4].trim() === 'class="simple_form'
+            (data.split(" ")[1].trim() === 'class="action-content">' &&
+              currentTurn === "false") ||
+            (data.split(" ")[1].trim() === 'class="form-container">' &&
+              currentTurn === "true")
           ) {
             document.querySelector(".action").innerHTML = data;
-            console.log("action update");
+            // console.log("action update");
           } else {
-            console.log(data.split(" ")[0].trim());
+            // console.log(data.split(" ")[0].trim());
             players.forEach((player) => {
               if (
                 data.split(" ")[0].trim() ===
                 `<h1>${player.innerText.trim()}</h1>`
               ) {
-                console.log("match");
+                // console.log("match");
                 player.innerHTML = data;
               }
             });

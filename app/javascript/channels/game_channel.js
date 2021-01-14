afefcf61b4d968db6e6d7da7cf73f15a3f7dfa83
Seigo Jinbo <seigojinbo@gmail.com>
2021-01-14 15:07:11 +0900

    cleaned code

--- app/javascript/channels/game_channel.js
+++ app/javascript/channels/game_channel.js
@@ -7,43 +7,37 @@ const initGameCable = () => {
     const id = gameContainer.dataset.gameId;
     const players = document.querySelectorAll(".player");
     const action = document.querySelector(".action");
-
+    const user = action.dataset.user;
+    let current = "";
     consumer.subscriptions.create(
       { channel: "GameChannel", id: id },
       {
         received(data) {
-          const turn = document.querySelector(".turn");
-          const currentTurn = turn.dataset.currentTurn;
-          if (currentTurn === "true") {
-            console.log("yes");
-          }
-          if (currentTurn === "false") {
-            console.log("no");
-          }
-          // console.log(currentTurn);
-          // console.log(data.split(" "));
-          // console.log(data.split(" ")[1].trim() == 'class="action-content"');
           if (data[4] === "c") {
             const currentBet = document.querySelector(".current-bet");
             currentBet.innerHTML = data;
           } else if (
             data.split(" ")[1].trim() === "loses" ||
             (data.split(" ")[1].trim() === 'class="action-content">' &&
-              currentTurn === "false") ||
+              current != user) ||
             (data.split(" ")[1].trim() === 'class="form-container">' &&
-              currentTurn === "true")
+              current === user)
           ) {
+            console.log(current == user);
             document.querySelector(".action").innerHTML = data;
-            // console.log("action update");
           } else {
-            // console.log(data.split(" ")[0].trim());
             players.forEach((player) => {
               if (
                 data.split(" ")[0].trim() ===
                 `<h1>${player.innerText.trim()}</h1>`
               ) {
-                // console.log("match");
                 player.innerHTML = data;
+                if (data.length > 652) {
+                  current = data
+                    .split(" ")[0]
+                    .trim()
+                    .replace(/<\/?[^>]+(>|$)/g, "");
+                }
               }
             });
           }

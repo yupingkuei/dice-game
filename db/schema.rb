0f1275e8fef62428d7a47928618bcbf08860eb50
Seigo Jinbo <seigojinbo@gmail.com>
2021-01-19 15:23:05 +0900

    set up cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_065917) do
+ActiveRecord::Schema.define(version: 2021_01_19_040504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "games", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -24,10 +45,8 @@ ActiveRecord::Schema.define(version: 2021_01_08_065917) do
     t.string "rotation", default: [], array: true
     t.integer "quantity", default: 0
     t.integer "value", default: 2
-
     t.integer "turn", default: 0
     t.integer "max", default: 4
-
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
@@ -54,6 +73,7 @@ ActiveRecord::Schema.define(version: 2021_01_08_065917) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "games", "users"
   add_foreign_key "sessions", "games"
   add_foreign_key "sessions", "users"
